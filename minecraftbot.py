63c8e9d45dd204c9c493f66a5545484bc56b91ee
dean McRobie <dean.mcrobie@gmail.com>
2020-05-27 12:34:33 -0400

    code structure and dependencies update

--- minecraftbot.py
+++ minecraftbot.py
@@ -1,15 +1,38 @@
 import os
-
 import discord
+import logging
 from dotenv import load_dotenv
+from pythonjsonlogger import jsonlogger
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+def setup_logging(log_level):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(log_level)
+    json_handler = logging.StreamHandler()
+    formatter = jsonlogger.JsonFormatter(
+        fmt='%(asctime)s %(levelname)s %(name)s %(message)s'
+    )
+    json_handler.setFormatter(formatter)
+    logger.addHandler(json_handler)
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
 
-client.run(TOKEN)
+class BlockBotClient(discord.Client):
+    def __init__(self):
+        super().__init__()
+        self.log = logging.getLogger(__name__)
+
+    async def on_ready(self):
+        self.log.info('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        self.log.info('Message from {0.author}: {0.content}'.format(message))
+
+
+if __name__ == "__main__":
+    setup_logging(logging.INFO)
+    load_dotenv()
+
+    client = BlockBotClient()
+
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    client.run(TOKEN)
