e0ffce85a39c09f574344b0079737e0b2933bf48
dean McRobie <dean.mcrobie@gmail.com>
2020-05-29 13:08:32 -0400

    added better rules!

--- minecraftbot.py
+++ minecraftbot.py
@@ -37,10 +37,7 @@ class BlockBotClient(discord.Client):
     async def on_message(self, message):
         self.lazy_init()
         self.log.debug('Message from {0.author}: {0.clean_content}'.format(message))
-        for mention in message.mentions:
-            if mention == self.user:
-                self.log.debug("its for us!")
-                await self.process(message)
+        await self.process(message)
 
     async def status(self, message):
         # 'status' is supported by all Minecraft servers that are version 1.7 or higher.
@@ -49,17 +46,17 @@ class BlockBotClient(discord.Client):
                 await message.add_reaction(self.eyes)
                 self.log.debug("pinging")
                 status = self.mc.status()
-                smsg = "The server at {2} is up and has {0} players and replied in {1} ms".format(status.players.online, status.latency, self.uri)
+                smsg = "The server at `{2}` is up and has {0} players and replied in {1} ms".format(status.players.online, status.latency, self.uri)
                 self.log.info(smsg)
                 await message.add_reaction(self.thumbsup)
                 await message.channel.send(smsg)
         except Exception as err:
             self.log.error(err)
             await message.add_reaction(self.thumbsdown)
-            await message.channel.send("nope, doesn't look like the server at {} is up! {}".format(self.uri, err))
+            await message.channel.send("nope, doesn't look like the server at `{}` is up! {}".format(self.uri, err))
 
     async def process(self, message):
-        if '?' in message.content:
+        if '?' in message.clean_content and 'server' in message.clean_content.lower():
             # assume we are asking about the server, check connectivity
             self.log.debug("found a status question")
             await self.status(message)
